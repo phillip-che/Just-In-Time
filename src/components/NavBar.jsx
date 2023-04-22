1c98263da4846b72fe219fb21417ec2099eb1527
Leo Liu <leoxiyuliu@gmail.com>
2023-04-22 07:46:04 -0700

    implement starter nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
+import {Link} from "react-router-dom"
+import logo from '../assets/JIT_long_logo.png'
+import SearchBar from './SearchBar'
+import '../styles/NavBar.css'
+
 const NavBar = () => {
     return (
-        <div>
-            NAV BAR
+        <div className="nav-bar">
+            <div className="hamburger">
+                {/* <img/> */}
+            </div>
+            <div className="island">
+                <Link to="/">
+                    <img className="nav-bar-logo" src={logo} alt="Just in time logo"/>
+                </Link>
+            </div>
+            <SearchBar/>
         </div>  
     )
 }
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,10 +1,11 @@
+import NavBar from '../components/NavBar.jsx'
 import {Outlet} from "react-router-dom"
 
 const Layout = () => {
     return (
         <div>
             <nav>
-                
+            <NavBar /> 
             </nav>
             <Outlet />
         </div>
