032d5c72e4b9ed61672d983173c18fa237666dc5
Kyle Pu <kylez.pu@gmail.com>
2023-04-22 11:38:30 -0700

    Profile page

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js';
+// import { getAuth } from "firebase/auth";
+
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCBG_desmzOFPIrCSwjVygGl33eFGvcjkw",
+//     authDomain: "just-in-time-6cc0e.firebaseapp.com",
+//     projectId: "just-in-time-6cc0e",
+//     storageBucket: "just-in-time-6cc0e.appspot.com",
+//     messagingSenderId: "520654273648",
+//     appId: "1:520654273648:web:c9c95f455f130214f748b5",
+//     measurementId: "G-JEKDB2N2BQ"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Exports
+// const db = getFirestore(app);
+// const auth = getAuth(app);
+
+// export { auth, db }
