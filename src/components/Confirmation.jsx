a581e6aad1a69afc2791d8ce7871387b966b65ae
phillip-che <pche@cpp.edu>
2023-04-22 19:13:34 -0700

    Add method to get  order from database

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import { useParams } from "react-router";
 
 const Confirmation = () => {
 
     let params = useParams();
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const getOrder = async () =>  {
+            await supabase
+            .from('Orders')
+            .select()
+            .eq('order_id', params.orderID)
+            .then((response) => {
+                console.log(response.data);
+                setOrder(response.data);
+            })
+        }
+        getOrder();
+    })
 
     return (
         <div>
