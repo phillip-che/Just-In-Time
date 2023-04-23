bfa2aa268e99f0638bea1d9a3057d08a95ee80d4
Leo Liu <leoxiyuliu@gmail.com>
2023-04-23 07:42:47 -0700

    to pull

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
       <Link color="inherit" href="https://github.com/phillip-che/Just-In-Time">
-        Just in time.
+        Just in time
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
