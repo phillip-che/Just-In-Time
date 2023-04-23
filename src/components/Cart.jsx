86d0e263540966348bfb2f3e2596f54adf3cccd2
Kyle Pu <kylez.pu@gmail.com>
2023-04-23 01:08:13 -0700

    Change quantity in shopping cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,6 +13,8 @@ const Cart = () => {
 
   const [selectedOption, setSelectedOption] = useState(null);
 
+  const [reload, setReload] = useState(false);
+
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
@@ -23,23 +25,23 @@ const Cart = () => {
     // add code to submit payment information here
   };
 
-  useEffect(() => {    
+  useEffect(() => {
     // sets user
     supabase.auth.onAuthStateChange((event, session) => {
       console.log(session);
       if (session) {
         setUser(session.user);
         const getCartItems = async () => {
-            await supabase
-              .from('Carts')
-              .select()
-              .eq('user_id', session.user.id)
-              .then((response) => {
-                console.log(response.data);
-                setCartItems(response.data);
-              });
-          }
-          getCartItems();
+          await supabase
+            .from('Carts')
+            .select()
+            .eq('user_id', session.user.id)
+            .then((response) => {
+              console.log(response.data);
+              setCartItems(response.data);
+            });
+        }
+        getCartItems();
       }
     });
   }, []);
@@ -60,6 +62,20 @@ const Cart = () => {
       })
   }
 
+  const changeQuantity = (item, ind, change) => {
+    setReload(!reload);
+    console.log(item.quantity);
+
+    if(item.quantity === 0 && change === -1){
+      return;
+    }
+
+    let temp = cartItems.slice();
+    temp[ind] = { ...item, quantity: parseInt(item.quantity) + change };
+    setCartItems(temp);
+    console.log(item.quantity);
+  }
+
   return (
     <div className="cart-container">
       <div className="cart-sections">
@@ -67,22 +83,26 @@ const Cart = () => {
           <h2>Shopping Cart</h2>
 
           {cartItems ? (
-                      <div>
-                          {cartItems.map((item) => (
-                              <div className="cart-item shopping-cart-row" key={item.id}>
-                                  <div className="cart-item-name">{item.product_name}</div>
-                                  <div>{`$${item.price}`}</div>
-                                  {/* STYLE THIS */}
-                                  <div> - </div>
-                                  <div className="cart-item-quantity">{item.quantity}</div>
-                                  <div> + </div>
-                                  <div className="cart-item-remove" onClick={() => removeItem(item.id)}>
-                                        X
-                                  </div>
-                              </div>
-                          ))}
-                      </div>
-                  ) : null}
+            <div>
+              {cartItems.map((item, index) => (
+                <div className="shopping-list-item-container">
+                  <span className="cart-item shopping-cart-row" key={item.id}>
+                    <div className="cart-item-name">{item.product_name}</div>
+                    <div>{`$${item.price}`}</div>
+                    {/* STYLE THIS */}
+                  </span>
+                  <span className="modifications key={item.id}">
+                    <button onClick={() => changeQuantity(item, index, -1)}> - </button>
+                    <span className="cart-item-quantity">{item.quantity}</span>
+                    <button onClick={() => changeQuantity(item, index, 1)}> + </button>
+                    <button className="cart-item-remove" onClick={() => removeItem(item.id)}>
+                      X
+                    </button>
+                  </span>
+                </div>
+              ))}
+            </div>
+          ) : null}
 
           <div className="cart-total">Total: ${totalPrice.toFixed(2)}</div>
         </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
+// import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar(props) {
   const { onChange } = props;
